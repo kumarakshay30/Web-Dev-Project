4ebc00c44722d2e9884a61aea547f5516723c0d6
kumarakshay30 <akshayk098765@gmail.com>
2025-08-09 01:09:55 +0530

    Fix build script and add missing packages

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python src/manage.py collectstatic --no-input
-python src/manage.py migrate
+python manage.py collectstatic --no-input
+python manage.py migrate
